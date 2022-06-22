746ccfb0d8238020d8d651cbb4bc2a0b8782994c
Lars Grefer <lars.grefer@materna.de>
2022-06-22 18:19:40 +0200

    MavenRepoPublish

---
+++ gsb-build-plugin/src/main/java/de/bund/gsb/gradle/plugins/maven/MavenRepositoryPlugin.java
@@ -0,0 +1,97 @@
+package de.bund.gsb.gradle.plugins.maven;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.file.Directory;
+import org.gradle.api.internal.artifacts.BaseRepositoryFactory;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenPublication;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
+import org.gradle.api.publish.maven.tasks.PublishToMavenRepository;
+import org.gradle.api.tasks.Delete;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.api.tasks.bundling.Zip;
+
+import javax.inject.Inject;
+
+public class MavenRepositoryPlugin implements Plugin<Project> {
+
+    private final BaseRepositoryFactory repositoryFactory;
+
+    private Provider<Directory> repoDir;
+    private MavenArtifactRepository mavenRepository;
+    private MavenPublication repositoryPublication;
+    private TaskProvider<Delete> cleanLocalMavenRepo;
+    private TaskProvider<Task> publishAllTask;
+
+
+    @Inject
+    public MavenRepositoryPlugin(BaseRepositoryFactory repositoryFactory) {
+        this.repositoryFactory = repositoryFactory;
+    }
+
+
+    @Override
+    public void apply(Project project) {
+
+        repoDir = project.getLayout().getBuildDirectory().dir("maven-repo");
+
+        mavenRepository = repositoryFactory.createMavenRepository();
+        mavenRepository.setUrl(repoDir);
+        mavenRepository.setName("localMavenRepo");
+
+        cleanLocalMavenRepo = project.getTasks().register("cleanLocalMavenRepo", Delete.class, task -> {
+            task.delete(repoDir);
+        });
+
+        publishAllTask = project.getTasks().register("publishToLocalMavenRepo", task -> {
+            task.setGroup("publishing");
+        });
+
+        TaskProvider<Zip> mavenRepoZip = project.getTasks().register("mavenRepoZip", Zip.class, zipTask -> {
+            zipTask.from(repoDir);
+            zipTask.dependsOn(publishAllTask);
+            zipTask.getArchiveClassifier().set("mavenRepo");
+            zipTask.getDestinationDirectory().convention(project.getLayout().getBuildDirectory());
+        });
+
+        project.getPlugins().apply("maven-publish");
+
+        PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
+
+        repositoryPublication = publishing.getPublications().create("mavenRepo", MavenPublication.class, mrp -> {
+            mrp.artifact(mavenRepoZip);
+        });
+
+        project.getTasks().withType(PublishToMavenRepository.class).configureEach(ptmr -> {
+            ptmr.onlyIf(t -> !(ptmr.getRepository() == mavenRepository && ptmr.getPublication() == repositoryPublication));
+        });
+
+        project.allprojects(this::configureProject);
+    }
+
+    private void configureProject(Project project) {
+        project.getPlugins().withType(MavenPublishPlugin.class, mpp -> {
+            PublishingExtension publishingExtension = project.getExtensions().findByType(PublishingExtension.class);
+
+            publishingExtension.getRepositories().add(mavenRepository);
+
+            publishAllTask.configure(pat -> {
+                pat.dependsOn(project.getTasks().withType(PublishToMavenRepository.class)
+                        .matching(ptmr -> ptmr.getRepository() == mavenRepository && ptmr.getPublication() != repositoryPublication));
+            });
+
+        });
+
+        project.afterEvaluate(sp -> {
+            project.getTasks().withType(PublishToMavenRepository.class).configureEach(ptmr -> {
+                if (ptmr.getRepository() == mavenRepository) {
+                    ptmr.dependsOn(cleanLocalMavenRepo);
+                }
+            });
+        });
+    }
+}
