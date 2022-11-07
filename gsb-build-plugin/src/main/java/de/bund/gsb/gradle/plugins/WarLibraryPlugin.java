7e71c3c45fdf18778b7c525d77e627c0b0e41481
Lars Grefer <lars.grefer@materna.de>
2022-11-07 17:07:15 +0100

    War-Library Plugin separat deklarieren

--- gsb-build-plugin/src/main/java/de/bund/gsb/gradle/plugins/WarLibraryPlugin.java
+++ gsb-build-plugin/src/main/java/de/bund/gsb/gradle/plugins/WarLibraryPlugin.java
@@ -5,6 +5,8 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.plugins.JavaLibraryPlugin;
 import org.gradle.api.plugins.WarPlugin;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
 
 public class WarLibraryPlugin implements Plugin<Project> {
 
@@ -15,8 +17,11 @@ public class WarLibraryPlugin implements Plugin<Project> {
         project.getPlugins().apply(JavaLibraryPlugin.class);
 
         Configuration providedApi = project.getConfigurations().create("providedApi");
+
+        SourceSet main = project.getExtensions().getByType(SourceSetContainer.class).getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+
         Configuration providedCompile = project.getConfigurations().getByName(WarPlugin.PROVIDED_COMPILE_CONFIGURATION_NAME);
-        Configuration api = project.getConfigurations().getByName("api");
+        Configuration api = project.getConfigurations().getByName(main.getApiConfigurationName());
 
         api.extendsFrom(providedApi);
         providedCompile.extendsFrom(providedApi);
