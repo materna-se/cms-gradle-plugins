f222f399a9f64c1c3a898b85d01ea0c424b63f40
Lars Grefer <lars.grefer@materna.group>
2026-06-01 17:32:55 +0200

    Trivy and CycloneDx support for sbom plugin

--- cms-build-plugin/src/main/java/de/materna/cms/gradle/plugins/component/CmsComponentPlugin.java
+++ cms-build-plugin/src/main/java/de/materna/cms/gradle/plugins/component/CmsComponentPlugin.java
@@ -26,6 +26,7 @@ import de.materna.cms.gradle.plugins.idea.IdeaUtils;
 import de.materna.cms.gradle.plugins.jib.JibSemanticTagsPlugin;
 import de.materna.cms.gradle.plugins.jib.JibUtil;
 import de.materna.cms.gradle.plugins.sbom.SbomPlugin;
+import de.materna.cms.gradle.plugins.trivy.TrivyDistributionPlugin;
 import io.freefair.gradle.util.GitUtil;
 import lombok.RequiredArgsConstructor;
 
@@ -201,6 +202,7 @@ public abstract class CmsComponentPlugin implements Plugin<Project> {
         project.getPlugins().withId("com.google.cloud.tools.jib", this::configureJib);
 
         project.getPlugins().withId("org.cyclonedx.bom", this::configureCycloneDx);
+        project.getPlugins().withType(TrivyDistributionPlugin.class, this::configureTrivy);
 
         project.getPlugins().apply(DuplicateStartScriptsPlugin.class);
 
@@ -575,6 +577,11 @@ public abstract class CmsComponentPlugin implements Plugin<Project> {
             }
 
         });
+    }
+
+
+    private void configureTrivy(TrivyDistributionPlugin trivyDistributionPlugin) {
+        project.getPlugins().apply(SbomPlugin.class);
     }
 
     private void configureCycloneDx(Plugin<?> plugin) {
